d7fd8b91f39c40b6f016bcafadaf40ffc64fe6dd
strjonas <jonas.strabel@gmail.com>
2021-05-07 23:39:00 +0200

    added firebase, worked ones but now it throws an error

--- src/components/PhotoHandler.js
+++ src/components/PhotoHandler.js
@@ -1,11 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MdFileUpload } from "react-icons/md";
 import { useDropzone } from "react-dropzone";
-import { FileInput } from "react-md";
+import { firebaseApp } from "./../base";
 
 export default function PhotoHandler() {
   const [files, setFiles] = useState([]);
+  const [pictures, setPictures] = useState([]);
 
+  useEffect(() => getPictures(), []);
+
+  async function upload(acceptedFiles) {
+    const upFile = acceptedFiles;
+    if (upFile === null || upFile === undefined) return;
+    const storageRef = firebaseApp.storage().ref();
+    const fileRef = storageRef.child(upFile.name);
+    await fileRef.put(upFile).then(() => {
+      console.log("uploaded file");
+    });
+    await fileRef.getDownloadURL().then(function (url) {
+      addPicture(url);
+      console.log(url);
+    });
+  }
+
+  async function deletePicture(id) {
+    const body = { id };
+    await fetch("http://192.168.178.41:5000/pictures", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+
+    console.log("deleted picture");
+  }
+  async function getPictures() {
+    const response = await fetch("http://192.168.178.41:5000/pictures");
+
+    const jsonData = await response.json();
+    console.log(jsonData);
+    setPictures(jsonData);
+    console.log("getPictures");
+  }
+  async function addPicture(url) {
+    const body = { url };
+    await fetch("http://192.168.178.41:5000/pictures", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    console.log("addPicture");
+  }
   const { getRootProps, getInputProps } = useDropzone({
     accept: "image/*",
     onDrop: (acceptedFiles) => {
@@ -16,6 +60,8 @@ export default function PhotoHandler() {
           })
         )
       );
+      console.log(acceptedFiles);
+      upload(acceptedFiles);
     },
   });
 
